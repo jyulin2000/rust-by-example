8053261f00c4007db2fff803855e0cae336a6424
jyulin2000 <jyulin2000@gmail.com>
2021-11-12 19:58:33 -0500

    Initial commit

---
+++ chapter1_hello_world/target/debug/chapter1_hello_world.d
@@ -0,0 +1,1 @@
+/Users/lilychen/Projects/rust-sandbox/rust-by-example/chapter1_hello_world/target/debug/chapter1_hello_world: /Users/lilychen/Projects/rust-sandbox/rust-by-example/chapter1_hello_world/src/main.rs
---
+++ chapter1_hello_world/target/debug/deps/chapter1_hello_world.d
@@ -0,0 +1,5 @@
+/Users/lilychen/Projects/rust-sandbox/rust-by-example/chapter1_hello_world/target/debug/deps/chapter1_hello_world: src/main.rs
+
+/Users/lilychen/Projects/rust-sandbox/rust-by-example/chapter1_hello_world/target/debug/deps/chapter1_hello_world.d: src/main.rs
+
+src/main.rs:
