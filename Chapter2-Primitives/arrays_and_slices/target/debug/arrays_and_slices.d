ac7746b1a4c350a0ce8edf5134a2203b72fdfdfb
jyulin2000 <jyulin2000@gmail.com>
2021-11-12 20:24:58 -0500

    Fixed error in arrays and slices

---
+++ Chapter2-Primitives/arrays_and_slices/target/debug/arrays_and_slices.d
@@ -0,0 +1,1 @@
+/Users/lilychen/Projects/rust-sandbox/rust-by-example/Chapter2-Primitives/arrays_and_slices/target/debug/arrays_and_slices: /Users/lilychen/Projects/rust-sandbox/rust-by-example/Chapter2-Primitives/arrays_and_slices/src/main.rs
---
+++ Chapter2-Primitives/arrays_and_slices/target/debug/deps/arrays_and_slices.d
@@ -0,0 +1,5 @@
+/Users/lilychen/Projects/rust-sandbox/rust-by-example/Chapter2-Primitives/arrays_and_slices/target/debug/deps/arrays_and_slices: src/main.rs
+
+/Users/lilychen/Projects/rust-sandbox/rust-by-example/Chapter2-Primitives/arrays_and_slices/target/debug/deps/arrays_and_slices.d: src/main.rs
+
+src/main.rs:
