c3b5896f6567a4c10f9a4ef22f01dfa4d85e3dc2
jyulin2000 <jyulin2000@gmail.com>
2021-11-18 18:07:57 -0500

    Experimented with destructuring tuples and enums with match statements

---
+++ Chapter8-Flow-of-Control/destructuring/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let triple = (32, -2, 3);
+    // TODO ^ Try different values for `triple`
+
+    println!("Tell me about {:?}", triple);
+    // Match can be used to destructure a tuple
+    match triple {
+        // Destructure the second and third elements
+        (0, y, z) => println!("First is `0`, `y` is {:?}, and `z` is {:?}", y, z),
+        (1, ..)  => println!("First is `1` and the rest doesn't matter"),
+        (.., y, 3) => {
+            println!("Uh does this work");
+            let temp: String = format!("Hello, the second number times two is {}", 2*y);
+            println!("{}", temp)
+        }
+        // `..` can be used to ignore the rest of the tuple
+        _      => println!("It doesn't matter what they are"),
+        // `_` means don't bind the value to a variable
+    }
+}
+
