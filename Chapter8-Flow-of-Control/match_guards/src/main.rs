a42eaa0ba27c36aca61eed603fad17a829c439a4
jyulin2000 <jyulin2000@gmail.com>
2021-11-19 02:38:06 -0500

    Played around with match guards

---
+++ Chapter8-Flow-of-Control/match_guards/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let pair = (2, 4);
+    // TODO ^ Try different values for `pair`
+
+    println!("Tell me about {:?}", pair);
+    match pair {
+        (x, y) if x == y => println!("These are twins"),
+        // The ^ `if condition` part is a guard
+        (x, y) if x + y == 0 => println!("Antimatter, kaboom!"),
+        (x, _) if x % 2 == 1 => println!("The first one is odd"),
+        (x, y) => println!("Bleh"),
+        //_ => println!("No correlation..."),
+    }
+}
+
